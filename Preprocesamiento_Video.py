9198bf198c892199f329d27615bc9e93e143beaf
Santidnp <sngh966.sg@gmail.com>
2021-10-02 23:16:20 -0500

    Inicio del proyecto

---
+++ Preprocesamiento_Video.py
@@ -0,0 +1,25 @@
+import cv2
+
+camera = cv2.VideoCapture('GOPR7428.MP4')
+
+camera.set(cv2.CAP_PROP_POS_FRAMES, 500)
+#
+# ret = True
+# fps = camera.get(cv2.CAP_PROP_FPS)
+# print(fps)
+# while ret:
+#     ret, image = camera.read()
+#     if ret:
+#         cv2.imshow("Image", image)
+#         cv2.waitKey(int(1000 / fps))
+
+i = 0
+while (camera.isOpened()):
+    ret, frame = camera.read()
+    if ret == False:
+        break
+    cv2.imwrite('kang' + str(i) + '.jpg', frame)
+    i += 1
+
+camera.release()
+cv2.destroyAllWindows()
